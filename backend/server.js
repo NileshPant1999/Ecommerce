9aa72a18060596066d0de285790d930471fb6940
Nilesh Pant <nileshpant6969@gmail.com>
2020-12-19 18:53:47 +0530

    added backend

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,15 @@ app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
@@ -15,5 +24,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server runnung at port ${port}`);
+  console.log(`Serve at http://localhost:${port}`);
 });
