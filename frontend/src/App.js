5a5a1d8476e025f1bb281dcceb06847e7ef0d4d6
Nilesh Pant <nileshpant6969@gmail.com>
2020-12-23 15:06:26 +0530

    added cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Link, Route } from "react-router-dom";
 import ProductScreen from "./screen/ProductScreen";
 import HomeScreen from "./screen/HomeScreen";
 import CartScreen from "./screen/CartScreen";
+import { useSelector } from "react-redux";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">
+            <Link className="brand" to="/">
               Harshitona
-            </a>
+            </Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
